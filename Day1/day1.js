c20bd6a1eae18679add0fb588d10e03a36315a90
Subhransu Sekhar Rath <ssrath38@gmail.com>
2024-07-13 08:07:35 +0530

    Day1 Complete

---
+++ Day1/day1.js
@@ -0,0 +1,28 @@
+var num = 10;
+console.log("num", num);
+let str = "Hello JS!";
+console.log("str", str);
+const bool = true;
+console.log("bool", bool);
+
+let var1 = 12;
+let var2 = "HI";
+let var3 = false;
+const var4 = {
+  name: "SSRath",
+  age: 25,
+};
+
+const var5 = ["apple", "banana", "mango"];
+
+console.log(typeof var1, var1);
+console.log(typeof var2, var2);
+console.log(typeof var3, var3);
+console.log(typeof var4, var4);
+console.log(typeof var5, var5);
+
+num = 15;
+console.log("num", num);
+
+// bool = false;
+// console.log("bool", bool);
