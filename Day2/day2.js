8f41fb8c55feadb52e76c5c633300d3bf8a21e9a
Subhransu Sekhar Rath <ssrath38@gmail.com>
2024-07-14 09:32:22 +0530

    Day2 Complete

---
+++ Day2/day2.js
@@ -0,0 +1,24 @@
+console.log("addition: 38 + 67 = ", 38 + 67);
+console.log("subtraction: 38 - 67 = ", 38 - 67);
+console.log("multiplication: 38 * 67 = ", 38 * 67);
+console.log("division: 38 / 67 = ", 38 / 67);
+console.log("remainder: 67 % 38 = ", 67 % 38);
+
+let var1 = 38;
+var1 += 67;
+console.log(var1);
+var1 -= 67;
+console.log(var1);
+
+console.log("38 > 67 ", 38 > 67);
+console.log("38 < 67 ", 38 < 67);
+console.log("38 <= 67 ", 38 <= 67);
+console.log("38 >= 67 ", 38 >= 67);
+console.log("38 == 67 ", 38 == 67);
+console.log("38 === 67 ", 38 === 67);
+
+console.log("38 == 67 && 38 != 68 ", 38 == 67 && 38 != 68);
+console.log("38 == 67 || 38 != 68 ", 38 == 67 || 38 != 68);
+console.log("!38 === 67 ", !(38 === 67));
+
+console.log(38 >= 0 ? "Positive" : "Negative");
